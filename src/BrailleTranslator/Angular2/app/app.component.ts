25f1fb501e34367868bcbf5cbc7dd1712d288768
msmereka <smereka.m@gmail.com>
2017-02-20 19:05:00 +0200

    Add custom checkboxes to dropdown, apply basic styles

--- src/BrailleTranslator/Angular2/app/app.component.ts
+++ src/BrailleTranslator/Angular2/app/app.component.ts
@@ -6,7 +6,6 @@ import { Component } from "@angular/core";
         <braille-navigation></braille-navigation>
         <router-outlet></router-outlet>
         <braille-footer></braille-footer>
-
     `
 })
 export class AppComponent { }
--- src/BrailleTranslator/Angular2/app/shared/components/dropdown.component.ts
+++ src/BrailleTranslator/Angular2/app/shared/components/dropdown.component.ts
@@ -9,22 +9,38 @@ import { KeyValuePair } from '../models/KeyValuePair';
 	styleUrls: ['./app/shared/components/dropdown.component.css']
 })
 export class DropdownComponent implements OnInit {
+	private resultView: string;
 	private isCollapsed: boolean = true;
-	private selectedItemIds: number[];
-	private items: KeyValuePair<number, string>[] = [
-		new KeyValuePair<number,string>(1, 'one'),
-		new KeyValuePair<number,string>(2, 'two'),
-		new KeyValuePair<number,string>(3, 'three'),
-		new KeyValuePair<number,string>(4, 'four')
+	private items: DropdownRow<number, string>[] = [
+		new DropdownRow<number,string>(1, 'one'),
+		new DropdownRow<number,string>(2, 'two'),
+		new DropdownRow<number,string>(3, 'three'),
+		new DropdownRow<number,string>(4, 'four')
 	];
 	constructor() { }
 
 	getResultView() {
-		return this.items.map(x => x.value).join(', ');
+		let a: string[] = [];
+		this.items.forEach(y => { if (y.checked) a.push(y.item.value) });
+		this.resultView = a.join(', ');
+	}
+
+	itemChecked(id: number) {
+		let index = this.items.map(x => x.item.key).indexOf(id);
+		this.items[index].checked = !this.items[index].checked;
+		this.getResultView();
 	}
 
 	ngOnInit() {
 	}
 };
 
+class DropdownRow<TKey, TValue> {
+	public checked: boolean = false;
+	public item: KeyValuePair<TKey, TValue>;
+
+	constructor(key: TKey, value:TValue) {
+		this.item = new KeyValuePair<TKey, TValue>(key, value);
+	}
+};
 
